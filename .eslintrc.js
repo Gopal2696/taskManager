d2d6cf8e1facb5dbc8080f3639dbe17af869847b
Gopal <gopal.airon@shipsy.co.in>
2020-04-11 17:41:56 +0530

    linting rules assed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,19 @@
 module.exports = {
-    "extends": "plugin:prettier/recommended",
-    "rules": {
-        "semi":["error", "always"],
-        "camelcase":"off",
-        "comma-dangle":"off",
-        "indent":["error", 2],
-        "prefer-const": ["error", {
-            "destructuring": "any",
-            "ignoreReadBeforeAssign": false
-        }]
+    extends: "plugin:prettier/recommended",
+    rules: {
+        trailingComma: "all",
+        bracketSpacing: true,
+        useTabs: true,
+        tabWidth: 4,
+        semi: true,
+        singleQuote: true,
+        arrowParens: "always",
     },
-    "parserOptions": {
-        "ecmaVersion": 2017
+    parserOptions: {
+        ecmaVersion: 2017,
     },
-    "parser": "babel-eslint",
-    "env": {
-        "es6": true
-    }
-  };
+    parser: "babel-eslint",
+    env: {
+        es6: true,
+    },
+};
